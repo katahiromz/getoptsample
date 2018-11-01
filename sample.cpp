7df49bfa41428fc4237f52de189770306391c566
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-11-01 13:42:04 +0900

    improve header

--- sample.cpp
+++ sample.cpp
@@ -1,4 +1,4 @@
-// getoptsample.cpp
+// getoptsample.cpp --- C++ getopt_long sample
 // Copyright (C) 2018 Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>.
 // This file is public domain software.
 
